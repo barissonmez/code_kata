0adc8a9d4c1c32bca9e6709fa6e8149633f34a73
barissonmez <private@barissonmez.net>
2020-08-14 01:08:17 +0300

    Kata_YourOrderPlase merged into master

---
+++ CodeWars/Kata.YourOrderPlease/Solution4.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.YourOrderPlease
+{
+    public class Solution4
+    {
+        public static string Order(string words)
+        {
+            return string.Join(" ", words
+                                                    .Split(' ')
+                                                    .OrderBy(a => a.SingleOrDefault(char.IsDigit)));
+        }
+    }
+}
--- CodeWars/Test.YourOrderPlease/YourOrderPleaseTests.cs
+++ CodeWars/Test.YourOrderPlease/YourOrderPleaseTests.cs
@@ -6,6 +6,14 @@ namespace Test.YourOrderPlease
     public class YourOrderPleaseTests
     {
         [Fact]
+        public void YourOrderPleaseSolution4Tests()
+        {
+            Assert.Equal("Thi1s is2 3a T4est", Kata.YourOrderPlease.Solution4.Order("is2 Thi1s T4est 3a"));
+            Assert.Equal("Fo1r the2 g3ood 4of th5e pe6ople", Kata.YourOrderPlease.Solution4.Order("4of Fo1r pe6ople g3ood th5e the2"));
+            Assert.Equal("", Kata.YourOrderPlease.Solution4.Order(""));
+        }
+
+        [Fact]
         public void YourOrderPleaseSolution3Tests()
         {
             Assert.Equal("Thi1s is2 3a T4est", Kata.YourOrderPlease.Solution3.Order("is2 Thi1s T4est 3a"));
